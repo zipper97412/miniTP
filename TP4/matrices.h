77930f77ff427f1def9aa6b3cb0ac37c9ddb9391
zipper97412 <zipper97412@gmail.com>
2015-10-15 19:45:54 +0200

    fin matrices

---
+++ TP4/matrices.h
@@ -0,0 +1,8 @@
+#ifndef MATRICES_H
+#define MATRICES_H
+
+void afficherMatrice(int* matrix, int m, int n);
+void additionnerMatrices(int* matrix1, int* matrix2, int* result, int m, int n);
+void transpose(int* matrix, int* result, int m, int n);
+void produitMatriciel(int* matrix1, int* matrix2, int* result, int m, int n);
+#endif
