8230ac76226e77e76fe15aa5987d5294b93d8752
zipper97412 <zipper97412@gmail.com>
2015-11-16 10:33:34 +0100

    doudou

--- TP9/chainList.c
+++ TP9/chainList.c
@@ -5,7 +5,7 @@
 
 void afficherListe(liste *l) {
 	element* cell;
-	for(cell = (element*)l;cell != NULL;cell = cell->suivant) {
+	for(cell = (element*)(*l);cell != NULL;cell = cell->suivant) {
 		printf("%d ",cell->valeur);
 	}
 	printf("\n");
@@ -13,34 +13,62 @@ void afficherListe(liste *l) {
 
 void insererElement(int x, liste *l) {
 	element* e;
+	element* cell;
 	if(( e = (element* ) malloc(sizeof(element)) ) == NULL) {
 		perror("erreur malloc");
 		exit(0);
 	}
-	e->suivant = (element*)l;
-	l = (liste)e;	
+	e->valeur = x;
+	if(*l == NULL) {
+		e->suivant = NULL;
+		*l = (liste)e;
+		return;
+	}
+	cell = (element*)(*l); 
+	while( (cell->suivant != NULL) && (cell->suivant->valeur < x) ){
+		cell = cell->suivant;
+	}
+	if(cell->suivant == NULL) {
+		cell->suivant = e;
+		e->suivant = NULL;
+	}
+	else if (cell == (element*)(*l)) {
+		e->suivant = cell;
+		*l = e;
+	}
+	else {
+		e->suivant = cell->suivant;
+		cell->suivant = e;
+	}
 }
 
 void supprimerElement(int i, liste *l) {
-	int k = 0;
+	int k = 1;
 	element* cell;
 	element* temp;
-	if(l == NULL) {
+	if(*l == NULL) {
 		return;
 	}
 	if(i == 0) {
-		temp = l;
-		l = temp->suivant;
+		temp = (element*)(*l);
+		*l = (liste)(temp->suivant);
 		free(temp);
 		return;
 	}
-		
-	for(cell = (element*)l;cell != NULL;cell = cell->suivant) {
-		if (k == i-1) {
-			temp = cell->suivant;
-			cell->suivant = cell->suivant->suivant;
-			free(temp);
-			return;
-		}
+	
+
+	cell = (element*)(*l); 
+	while(cell != NULL && k < i){
+		cell = cell->suivant;
+		k++;
+	}	
+	/* cell est le precedent de celui qu'il faut supprimer
+           Si k==i on a trouvé */
+	if (cell != NULL && cell->suivant != NULL){
+		temp = cell->suivant;
+		cell->suivant = temp->suivant;
+		free(temp);	
 	}
+	else{ /* element supprime inexistant */
+        }
 }
